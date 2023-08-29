527108057d07fb8ff0a78292092192d4cfc45f14
Cesar <cesar.m.rodriguez@tec.mx>
2023-08-28 19:28:08 -0600

    Update SwiftUI example

---
+++ 6) SwiftUIDesign/SwiftUIDesign/SwiftUIDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignApp.swift
+//  SwiftUIDesign
+//
+//  Created by César Manuel on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
